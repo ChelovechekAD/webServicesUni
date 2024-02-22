6d4eda38aea583b90f93429b9ecda8e67bb0bd8d
Chelovek <yushkevich2004@mail.ru>
2024-02-22 13:43:36 +0300

    123

---
+++ src/main/java/org/pztasks/TestEntity.java
@@ -0,0 +1,10 @@
+package org.pztasks;
+
+import org.pztasks.Annotations.FieldNames;
+
+@FieldNames
+public class TestEntity {
+    private Long id;
+    private String title;
+    private String phoneNumber;
+}
